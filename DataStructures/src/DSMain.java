4a1784044494c3847e69373f6320864552d812df
nevercoded <nevercoded@outlook.com>
2013-10-13 12:28:32 -0700

    Adding Data Structures Project

---
+++ DataStructures/src/DSMain.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: skumar
+ * Date: 10/13/13
+ * Time: 11:53 AM
+ * Main Class
+ */
+
+public class DSMain {
+
+    public static void main(String args[]){
+        System.out.println("Hello DS");
+    }
+}
